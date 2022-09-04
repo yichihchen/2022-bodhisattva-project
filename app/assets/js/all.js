3fc92225e132f708deed79c288c5d3633c5b145a
yichihchen <ingridiaim@gmail.com>
2022-09-04 11:26:01 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+
+
+$(".menu-hamburger-open").click(function () {
+  $(".menu-hamburger-open").hide()
+  $(".menu-hamburger-close").show()
+})
+
+$(".menu-hamburger-close").click(function () {
+  $(".menu-hamburger-close").hide()
+  $(".menu-hamburger-open").show()
+})
+
+$(".menu-hamburger-open,.menu-hamburger-close").click(function () {
+  $(".mobile-menu").toggleClass("active")   
+  //動態載入class如何使用??用toggleClass可以動,用show不能動??
+  //注意要寫active而非.active
+})
+
